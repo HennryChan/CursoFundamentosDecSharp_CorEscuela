17398f3b35ebcc6f46033ebcb83266486e7130e9
Hennry Chan <dual.hennry.chan@gmail.com>
2022-05-04 17:36:45 -0500

    Se creo el proyecto

---
+++ FundamentosCSharp_CorEscuela/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FundamentosCSharp_CorEscuela
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
