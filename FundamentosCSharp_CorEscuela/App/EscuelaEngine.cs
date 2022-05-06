13035b891115268346a665bb1f757042339f0301
Hennry Chan <dual.hennry.chan@gmail.com>
2022-05-06 18:39:30 -0500

    Refactorizar a escuela engine 25/32

---
+++ FundamentosCSharp_CorEscuela/App/EscuelaEngine.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FundamentosCSharp_CorEscuela.Entidades;
+
+namespace FundamentosCSharp_CorEscuela.App
+{
+    public class EscuelaEngine
+    {
+        public Escuela Escuela { get; set; }
+
+        public EscuelaEngine()
+        {
+
+        }
+
+        public void Inicializar()
+        {
+            Escuela = new Escuela("Platzi Acedemy", 2021, TiposEscuela.Primaria, cuidad: "Bogota", pais: "colombia");
+
+            Escuela.Cursos = new List<Curso>()
+            {
+                new Curso(){Nombre = "101", Jornada = TiposJornada.Tarde },
+                new Curso(){Nombre = "201", Jornada = TiposJornada.Tarde },
+                new Curso(){Nombre = "301", Jornada = TiposJornada.Tarde },
+                new Curso(){Nombre = "401", Jornada = TiposJornada.Tarde },
+                new Curso(){Nombre = "501", Jornada = TiposJornada.Tarde }
+            };
+
+        }
+    }
+
+}
--- FundamentosCSharp_CorEscuela/Program.cs
+++ FundamentosCSharp_CorEscuela/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using FundamentosCSharp_CorEscuela.Entidades;
+using FundamentosCSharp_CorEscuela.App;
 using static System.Console;
 
 namespace FundamentosCSharp_CorEscuela
@@ -9,81 +10,22 @@ namespace FundamentosCSharp_CorEscuela
     {
         static void Main(string[] args)
         {
-            var escuela = new Escuela("Platzi Acedemy", 2021, TiposEscuela.Primaria, cuidad: "Bogota", pais: "colombia");
-
-            escuela.Cursos = new List<Curso>()
-            {
-                new Curso(){Nombre = "101", Jornada = TiposJornada.Tarde },
-                new Curso(){Nombre = "201", Jornada = TiposJornada.Tarde },
-                new Curso(){Nombre = "301", Jornada = TiposJornada.Tarde }
-            };
-
-            escuela.Cursos.Add(new Curso() { Nombre = "102", Jornada = TiposJornada.Maniana });
-            escuela.Cursos.Add(new Curso() { Nombre = "202", Jornada = TiposJornada.Maniana });
-
-            var otrColeccion = new List<Curso>()
-            {
-                new Curso(){Nombre = "401", Jornada = TiposJornada.Tarde },
-                new Curso(){Nombre = "501", Jornada = TiposJornada.Tarde },
-                new Curso(){Nombre = "501", Jornada = TiposJornada.Tarde }
-            };
-
-            escuela.Cursos.AddRange(otrColeccion);
-            //escuela.Cursos.Add(temp);
-
-            ImprimirCursosEscuela(escuela);
-
-            //Predicate<Curso> miAlgoritmo = Predicado;
-            //escuela.Cursos.RemoveAll(miAlgoritmo);
-            WriteLine("--------------------------------");
-
-            ImprimirCursosEscuela(escuela);
-
-            /*escuela.Cursos.AddRange(otrColeccion);
-             * ImpimirCursosEscuela(escuela);
-             * escuela.Cursos.RemoveAll(delegate (Curso cur)
-                        {
-                            return cur.Nombre == "301";
-                        });
-            escuela.Cursos.RemoveAll((cur)=> cur.Nombre == "501" && cur.Jornada == TiposJornada.Mañana );
-*/
-
+            var engine = new EscuelaEngine();
+            engine.Inicializar();
+            ImprimirCursosEscuela(engine.Escuela);
         }
-
-        //private static bool Predicado(Curso curobj)
-        //{
-        //    return curobj.Nombre == "301";
-        //}
 
         private static void ImprimirCursosEscuela(Escuela escuela)
         {
-            WriteLine("-------------------------------");
-            WriteLine("Cursos de la escuela");
-            WriteLine("--------------------------------");
+            WriteLine("----------------------------------------------------");
+            WriteLine("              Cursos de la escuela                  ");
+            WriteLine("----------------------------------------------------");
             if (escuela.Cursos != null)
             {
                 foreach (var curso in escuela.Cursos)
                 {
                     WriteLine($"Nombre {curso.Nombre}, Id {curso.UniqueId}");
                 }
-            }
-        }
-
-        private static void ImprimirCursosWhile(Curso[] arregloCursos)
-        {
-            int contador = 0;
-            while (contador < arregloCursos.Length)
-            {
-                Console.WriteLine($"Nombre{arregloCursos[contador].Nombre}, Id {arregloCursos[contador].UniqueId}");
-                contador++;
-            }
-        }
-
-        private static void ImprimirCursosForeach(Curso[] arregloCursos)
-        {
-            foreach (var curso in arregloCursos)
-            {
-                Console.WriteLine($"Nombre{curso.Nombre}, Id {curso.UniqueId}");
             }
         }
     }
